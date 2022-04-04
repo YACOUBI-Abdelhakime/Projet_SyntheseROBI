23a714926b9642d5cbe3d83043a8a4513ecab82f
YACOUBI-Abdelhakime <hakim.199911@gmail.com>
2022-04-04 02:26:16 +0200

    v-4.1

--- Robi/src/exercice3/Exercice3_0.java
+++ Robi/src/exercice3/Exercice3_0.java
@@ -2,6 +2,7 @@ package exercice3;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Point;
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
@@ -10,6 +11,7 @@ import graphicLayer.GRect;
 import graphicLayer.GSpace;
 import stree.parser.SNode;
 import stree.parser.SParser;
+import tools.Tools;
 
 public class Exercice3_0 {
 	GSpace space = new GSpace("Exercice 3", new Dimension(200, 100));
@@ -57,8 +59,43 @@ public class Exercice3_0 {
 		cmd.run();
 	}
 
-	Command getCommandFromExpr(SNode expr) {
-		return null;
+	Command getCommandFromExpr(SNode expr){
+		Command command;
+		String target = expr.get(0).contents();
+		if (target.equals("space")) {
+		String cmd = expr.get(1).contents();
+		if (cmd.equals("setColor")){
+			SpaceChangeColor space= new SpaceChangeColor(Tools.getColorByName(expr.get(2).contents().toString()));
+			 command= space;
+		} else if (cmd.equals("sleep")){
+			//String val1= expr.get(2).contents().toString();
+            //int res1 = Integer.parseInt(val1);
+            Spacesleep space= new Spacesleep(Integer.parseInt(expr.get(2).contents().toString()));
+            command=space;
+		} else
+		throw new Error("Invalid space command:" + expr);
+		} else if (target.equals("robi")) {
+		String cmd = expr.get(1).contents();
+		if (cmd.equals("setColor")){
+			//String couleur= expr.get(2).contents().toString();
+            //Color c=Tools.getColorByName(couleur);
+           // robi.setColor(c);
+            RobiChangeColor robi= new  RobiChangeColor(Tools.getColorByName(expr.get(2).contents().toString()));
+            command= robi;
+		} else if (cmd.equals("translate")) {
+			//String val1= expr.get(2).contents().toString();
+            //String val2= expr.get(3).contents().toString();
+            //int res1 = Integer.parseInt(val1);
+            //int res2 = Integer.parseInt(val2);
+            Robitranslate robi= new Robitranslate(Integer.parseInt(expr.get(2).contents().toString()), Integer.parseInt(expr.get(3).contents().toString()));
+            //robi.translate(new Point(res1,res2));
+            command = robi;
+		} else {
+		throw new Error("Invalid robi command:" + expr);
+		}
+		} else
+		throw new Error("Unknown target in command: " + expr);
+		return command;
 	}
 
 	public static void main(String[] args) {
@@ -82,4 +119,55 @@ public class Exercice3_0 {
 		}
 
 	}
+	
+	public class RobiChangeColor implements Command {
+		Color newColor;
+
+		public RobiChangeColor(Color newColor) {
+			this.newColor = newColor;
+		}
+
+		@Override
+		public void run() {
+			robi.setColor(newColor);
+		}
+
+	}
+	
+	public class Spacesleep implements Command {
+		int s;
+
+		public Spacesleep(int s) {
+			this.s = s;
+		}
+
+		@Override
+		public void run() {
+			try {
+                Thread.sleep(s);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+		}
+
+	}
+	
+	public class Robitranslate implements Command {
+		int x, y;
+ 
+		public Robitranslate (int x, int y) {
+			this.x=x;
+			this.y=y;
+		}
+
+		@Override
+		public void run() {
+			robi.translate(new Point(x,y));
+			
+		}
+
+	}
+	
+	
 }
--- Robi/src/exercice4/Exercice4_1_0.java
+++ Robi/src/exercice4/Exercice4_1_0.java
@@ -1,7 +1,7 @@
 package exercice4;
 
 // 
-//	(space setColor black)  
+//	(space setColor yellow) (space sleep 1000) (robi translate 80 40) (robi setColor red)	
 //	(robi setColor yellow) 
 //	(space sleep 2000) 
 //	(space setColor white)  
@@ -18,10 +18,21 @@ package exercice4;
 //
 
 import java.awt.Dimension;
+import java.awt.Point;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import tools.Tools;
+import javax.sound.midi.Receiver;
 
+//import org.omg.CORBA.Environment;
+
+import java.awt.Color;
+
+import exercice3.Exercice3_0.Command;
+import graphicLayer.GElement;
 import graphicLayer.GRect;
 import graphicLayer.GSpace;
 import stree.parser.SNode;
@@ -42,12 +53,52 @@ public class Exercice4_1_0 {
 
 		Reference spaceRef = new Reference(space);
 		Reference robiRef = new Reference(robi);
+		
 
-		// Initialisation des references : on leur ajoute les primitives qu'elles
-		// comprenent
-		//
-		// <A VOUS DE CODER>
-		//
+		//AJOUT DES COMMANDES
+		spaceRef.addCommand("setColor", new Command() {
+			public Reference run(Reference ref, SNode espr) {
+				String couleur = espr.get(2).contents().toString();
+				Color c = Tools.getColorByName(couleur);
+				((GSpace) ref.receiver).setColor(c);
+
+				return ref;
+
+			}
+		});
+
+		robiRef.addCommand("setColor", new Command() {
+			public Reference run(Reference ref, SNode espr) {
+				String couleur = espr.get(2).contents().toString();
+				Color c = Tools.getColorByName(couleur);
+				((GElement) ref.receiver).setColor(c);
+
+				return ref;
+
+			}
+		});
+
+		spaceRef.addCommand("sleep", new Command() {
+			public Reference run(Reference ref, SNode espr) {
+				int res1 = Integer.parseInt(espr.get(2).contents());
+
+				// Thread.sleep(res1);
+				Tools.sleep(res1);
+
+				return ref;
+
+			}
+		});
+		robiRef.addCommand("translate", new Command() {
+			public Reference run(Reference ref, SNode espr) {
+				int res1 = Integer.parseInt(espr.get(2).contents());
+				int res2 = Integer.parseInt(espr.get(3).contents());
+				((GElement) ref.receiver).translate(new Point(res1, res2));
+
+				return ref;
+
+			}
+		});
 
 		// Enrigestrement des references dans l'environement par leur nom
 		environment.addReference("space", spaceRef);
@@ -89,8 +140,66 @@ public class Exercice4_1_0 {
 		receiver.run(expr);
 	}
 
+	public interface Command {
+		// le receiver est l'objet qui va executer method
+		// method est la s-expression resultat de la compilation
+		// du code source a executer
+		// exemple de code source : "(space setColor black)"
+		abstract public Reference run(Reference receiver, SNode method);
+	}
+
+	public class Reference {
+		Object receiver;
+		Map<String, Command> primitives;
+
+		public Reference(Object receiver) {
+
+			this.receiver = receiver;
+			primitives = new HashMap<String, Command>();
+		}
+
+		Command getCommandByName(String selector) {
+			Command c;
+			c = primitives.get(selector);
+			return c;
+		}
+
+		public void addCommand(String selector, Command primitive) {
+			primitives.put(selector, primitive);
+		}
+
+		public void run(SNode expr) {
+			Command cmd;
+			cmd = primitives.get(expr.get(1).contents());
+			try {
+			cmd.run(this, expr);
+			}catch(Exception e) {
+				System.err.println("Error : la s_expression n'est pas cohérante !!");
+			}
+		}
+	}
+
+	public class Environment {
+		HashMap<String, Reference> variables;
+
+		public Environment() {
+			variables = new HashMap<String, Reference>();
+		}
+
+		public void addReference(String name, Reference ref) {
+			variables.put(name, ref);
+		}
+
+		Reference getReferenceByName(String name) {
+			Reference ref;
+			ref = variables.get(name);
+
+			return ref;
+
+		}
+	}
+
 	public static void main(String[] args) {
 		new Exercice4_1_0();
 	}
-
 }
